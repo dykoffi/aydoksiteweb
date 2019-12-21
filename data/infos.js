21891f0178ed1eff6d00606ac09575377aebeaf0
dykoffi <koffiedy@gmail.com>
2019-12-21 18:56:12 +0000

    ajout de la pub

---
+++ data/infos.js
@@ -0,0 +1,62 @@
+exports.equipe = [
+    {
+        "nom": "Koffi Edy",
+        "profession": "Experts Genie Logiciel",
+        "mail": "koffiedy@gmail.com"
+    },
+    {
+        "nom": "Diegnana Ouattara",
+        "profession": "Marqueting & telecoms",
+        "mail": "oumou@gmail.com"
+    },
+    {
+        "nom": "Ouerdaogo Sara",
+        "profession": "UX Designer",
+        "mail": "sara@gmail.com"
+    },
+    {
+        "nom": "Kouakou Guy-Marc",
+        "profession": "Electronics & telecoms",
+        "mail": "guy@gmail.com"
+    },
+    {
+        "nom": "Diallo Adama",
+        "profession": "Electronics & telecoms",
+        "mail": "diallo@gmail.com"
+    }
+]
+
+exports.services = [
+    {
+        "nom": "Reseau Informatique",
+        "icon": "mdi-hardware-memory",
+        "details": "",
+        "lien": ""
+    },
+    {
+        "nom": "Logiciel Desktop",
+        "icon": "mdi-hardware-computer",
+        "details": "",
+        "lien": ""
+    },
+    {
+        "nom": "Application Android",
+        "icon": "mdi-action-android",
+        "details": "",
+        "lien": ""
+    },
+    {
+        "nom": "Installation d'electronique",
+        "icon": "mdi-action-settings-input-component",
+        "details": "",
+        "lien": ""
+    }
+    ,
+    {
+        "nom": "Site Web",
+        "icon": "mdi-action-language",
+        "details": "",
+        "lien": ""
+    }
+
+]
